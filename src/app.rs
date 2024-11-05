9cf2bc84920505e05129b1167a2bf1428ff5a536
yrncollo <ngenocollins599@gmail.com>
2024-11-05 14:15:29 +0300

    TUI setup

---
+++ src/app.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+use crossterm::event::{self, Event};
+
+pub struct App{
+    pub exit: bool,
+}
+
+impl App {
+    pub fn new() -> Self {
+        App { 
+            exit: false,
+        }
+        
+    }
+
+    pub fn handle_events(&mut self) -> io::Result<()>{
+        if let Event::Key(key) = event::read()?{
+            match key.code {
+                event::KeyCode::Char('q') => self.exit(),
+
+                _=> {}
+                
+            }
+        }
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+    
+}
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub mod app;
+mod ui;
+
+use std::io;
+use app::App;
+use ratatui::{prelude::Backend, Terminal};
+use ui::ui;
+
+pub fn launch_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool>{
+    loop {
+        terminal.draw(|frame| ui(frame, app))?;
+        app.handle_events()?;
+        if app.exit {
+            break;
+        }
+    }
+        Ok(false)
+}
